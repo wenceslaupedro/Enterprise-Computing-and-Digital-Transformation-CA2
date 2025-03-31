5268c8f6dce6a9e22fcf88343162dec068b16434
Pedro Wenceslau <58569347+wenceslaupedro@users.noreply.github.com>
2025-03-31 19:17:58 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,62 @@
 using Microsoft.EntityFrameworkCore;
 using WorkoutTracker.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace WorkoutTracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<WorkoutContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+            // Configure CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins(
+                            "https://fitnesstracker-h8duafahecdta0aa.westeurope-01.azurewebsites.net",
+                            "http://localhost:5000",
+                            "https://localhost:5001"
+                        )
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
 
+            // Configure Database
+            builder.Services.AddDbContext<WorkoutContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register WorkoutContext as a scoped service
+            builder.Services.AddScoped<WorkoutContext>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            // Configure default page
+            app.MapFallbackToFile("index.html");
+
+            app.Run();
+        }
+    }
+}
