46bdf2417b2c13d71510e048d3ae22f4bba180db
Pedro Wenceslau <pedro.wenceslau@gmail.com>
2025-04-08 19:38:44 +0100

    one more try

--- Program.cs
+++ Program.cs
@@ -10,21 +10,17 @@ namespace WorkoutTracker
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Register MVC controllers with the DI container
             builder.Services.AddControllers();
-            
-            // Add DbContext using SQL Server
+   
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            
-            // Add Swagger/OpenAPI support
+       
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WorkoutTracker API", Version = "v1" });
             });
 
-            // Add CORS policy
             builder.Services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
@@ -38,7 +34,6 @@ namespace WorkoutTracker
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -49,7 +44,6 @@ namespace WorkoutTracker
             app.UseCors();
             app.UseAuthorization();
             app.MapControllers();
-
             app.Run();
         }
     }
